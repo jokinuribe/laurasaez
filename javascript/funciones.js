cd47652ddb95b68219bb1c9c35396a191ece5d51
jokinuribe <jokin.uribe@gmail.com>
2022-04-07 19:47:01 +0200

    ofuscar email

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -5,8 +5,10 @@ j(document).ready(function(){
     j("#divFecha1").html(fecha_actual);
     j("#divFecha2").html(fecha_actual);
     j("#divFecha3").html(fecha_actual);
-    clientSideInclude('menu', './menu.html');
-
+    clientSideInclude('menu', './menu.html');	
+	ofuscado="e-mail: lau"+"ras"+"aez@la"+"uras"+"aez.com<br>";		
+	document.getElementById("divEmail").innerHTML=ofuscado;
+	
 });
 
 function menu(num){
@@ -104,3 +106,46 @@ if (req) {
             "compatible browsers may also exist.";
     }
 }
+
+/**
+ * Obfuscate a plaintext string with a simple rotation algorithm similar to
+ * the rot13 cipher.
+ * @param  {[type]} key rotation index between 0 and n
+ * @param  {Number} n   maximum char that will be affected by the algorithm
+ * @return {[type]}     obfuscated string
+ */
+String.prototype.ofuscate = function(key, n = 126) {
+  // return String itself if the given parameters are invalid
+  if (!(typeof(key) === 'number' && key % 1 === 0)
+    || !(typeof(key) === 'number' && key % 1 === 0)) {
+    return this.toString();
+  }
+
+  var chars = this.toString().split('');
+
+  for (var i = 0; i < chars.length; i++) {
+    var c = chars[i].charCodeAt(0);
+
+    if (c <= n) {
+      chars[i] = String.fromCharCode((chars[i].charCodeAt(0) + key) % n);
+    }
+  }
+
+  return chars.join('');
+};
+
+/**
+ * De-obfuscate an obfuscated string with the method above.
+ * @param  {[type]} key rotation index between 0 and n
+ * @param  {Number} n   same number that was used for obfuscation
+ * @return {[type]}     plaintext string
+ */
+String.prototype.desofuscate = function(key, n = 126) {
+  // return String itself if the given parameters are invalid
+  if (!(typeof(key) === 'number' && key % 1 === 0)
+    || !(typeof(key) === 'number' && key % 1 === 0)) {
+    return this.toString();
+  }
+
+  return this.toString().ofuscate(n - key);
+};
